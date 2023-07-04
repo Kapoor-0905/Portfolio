219e5dc42708a9c32d4811f3ee6eed7d6d3d09c9
Kapoor-0905 <500097111@stu.upes.ac.in>
2023-07-04 12:17:09 +0530

    initial changes

--- lib/utils/servicesUtils.dart
+++ lib/utils/servicesUtils.dart
@@ -17,7 +17,7 @@ class ServicesUtils {
 
   static const List<String> servicesDescription = [
     "- Started as Student Developer\n- Promoted to Lead Student Developer\n- Promoted to External Collaboration Lead\n- Mentored 2 Projects\n- Organized 3 Events\n- Speaker of GitHub Workshop",
-    "1. Government Funded Project-Department of Science and Technology-with UPES. \n",
+    "1. Government Funded Project-Department of Science and Technology-with UPES. \n2. Junior Flutter Developer : Curiosify",
     "- CSA Hackathon 1.0\n\n- SheBuilds\n\n- IBM CodeWars",
     "- Hacktoberfest 2022\n\n- GitHub",
   ];
